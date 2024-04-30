5669b2d0cca4b0548b45527ac3420db36b0523a7
Alexandra Camarena <alexandracamarena@ou.edu>
2024-04-29 22:26:13 -0500

    Add project files.

---
+++ LCP0501/Program.cs
@@ -0,0 +1,62 @@
+﻿// MIS 3013 001
+// May 1, 2024
+// Alexandra Camarena
+// 113568154
+Console.WriteLine("Exam 3 Practice");
+
+string menuStr = @"""
+********************************************
+1. Add a new student
+2. Show all students' information
+3. Search a student by ID
+4. Show the student with smallest age
+5. Show the average weight of all students
+********************************************
+    """;
+
+while (true) 
+{
+    Console.WriteLine(menuStr);
+
+    Console.Write($"Enter an option: ");
+    string userChoiceStr = Console.ReadLine();
+
+    if (userChoiceStr == "1")
+    {
+        // add a new student
+        Console.Write($"Input student ID: ");
+        string idStr = Console.ReadLine();
+
+        Console.Write($"Input student name: ");
+        string nameStr = Console.ReadLine();
+
+        Console.Write($"Input student age: ");
+        string ageStr = Console.ReadLine();
+        int ageInt = Convert.ToInt32( ageStr );
+
+        Console.Write($"Input student weight: ");
+        string weightStr = Console.ReadLine();
+        double weightDbl = Convert.ToDouble( weightStr );
+    }
+    else if (userChoiceStr == "2")
+    {
+        // show all students
+    }
+    else if (userChoiceStr == "3")
+    {
+        // search a student by ID
+    }
+    else if (userChoiceStr == "4")
+    {
+        // show student with smallest age
+    }
+    else if (userChoiceStr == "5")
+    {
+        // show average weight
+    }
+    else 
+    {
+        Console.WriteLine("Thnak you and goodbye!");
+        break;
+    }
+}
---
+++ LCP0501/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace a// folder a
+{
+    public class Student
+    {
+        public string ID;
+        public string Name;
+        public int Age;
+        public double Weight;
+
+        // end of class
+    }
+
+}
